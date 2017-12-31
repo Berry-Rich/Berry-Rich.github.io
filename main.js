57e71122abb5c3a2a19c9ded8b478aa040e444b3
Berry-Rich <31514788+Berry-Rich@users.noreply.github.com>
2017-12-31 14:45:02 -0800

    added effects to make text visible

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+  $('.projects').hide();
+  
+  $('.projects-button').on('click', function() {
+		$(this).next().slideToggle(400);
+    $(this).toggleClass('active');
+    $(this).text('Projects Viewed');
+	});
+}
 
+$(document).ready(main);
